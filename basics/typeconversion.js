c5cb6605d9706bbd876f8ace99a0e8be559cf435
Abhishek !!!! <147410102+Tony499370@users.noreply.github.com>
2024-07-26 20:34:57 +0000

    type conversion and operations

---
+++ basics/typeconversion.js
@@ -0,0 +1,22 @@
+let score = null
+
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+console.log(score)
+console.log(typeof score);
+
+let str1 = "tony"
+let str2 = "stark"
+
+console.log(str1 + str2);
+console.log("1" + 2 + 2);
+console.log(2 + 2 +"1");
+console.log(+true);
+
+    
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
