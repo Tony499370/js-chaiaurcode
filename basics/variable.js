8ba82a5de8a564c1b64673347a1a76a386e9bb18
Abhishek !!!! <147410102+Tony499370@users.noreply.github.com>
2024-07-26 06:16:44 +0000

    datatypes and variables

---
+++ basics/variable.js
@@ -0,0 +1,18 @@
+const accountId = 144521
+let accountEmail = "tony@email.com"
+var AccountPassword = "1235"
+accountCity = "jaipur"
+let accountState;
+
+// accountId = 5 not allowed
+// do not use var onlyn use let or const
+
+accountEmail = "a@ngj.com"
+AccountPassword = 59419
+accountCity = "amln"
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(AccountPassword);
+
+console.table([accountEmail , accountId , accountCity , AccountPassword , accountState])
